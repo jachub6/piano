7d71763d3f58077e7515bc879be154c27833360e
jachub6 <37016060+jachub6@users.noreply.github.com>
2018-11-17 22:36:38 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ var socketIO = require('socket.io');
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
-app.set('port', 8080);
+app.set('port', 50000);
 app.use('/static', express.static(__dirname + '/static'));
 app.use('/node_modules/howler/dist', express.static(__dirname + '/node_modules/howler/dist'));
+app.use('/node_modules/tone/build', express.static(__dirname + '/node_modules/tone/build'));
 app.use('/style', express.static(__dirname + '/style'));
 app.use('/noty', express.static(__dirname + '/noty'));
 
@@ -17,17 +18,45 @@ app.get('/', function(request, response) {
 });
 
 
-server.listen(8080, function() {
-    console.log('Starting server on port 8080');
+server.listen(50000, function() {
+    console.log('Starting server on port 50000');
 });
 var pocet=0;
+var nastaveniSynthu = {
+    "filter" : {
+        "type": "lowpass"
+    },
+    "oscillator" : {
+        "type": "sawtooth",
+        "detune" : 0
+    },
+    "envelope" : {
+        "attack" : 0.01 ,
+        "decay" : 0.1 ,
+        "sustain" : 0.1 ,
+        "release" : 3
+    }
+};
 io.on('connection', function(socket) {
     pocet++;
+    console.log(nastaveniSynthu);
+    socket.emit('zmenaNastaveni', nastaveniSynthu);
+
+    socket.on("zmenaNastaveni", function (data) {
+        nastaveniSynthu=data;
+        //console.log(nastaveniSynthu);
+        socket.broadcast.emit("zmenaNastaveni", data);
+    });
+
     io.sockets.emit('uspesne', pocet);
 
 
     socket.on("zahraj", function (data) {
         socket.broadcast.emit("zahraj", data);
+    });
+
+    socket.on("dohraj", function (data) {
+        socket.broadcast.emit("dohraj", data);
     });
 
     socket.on("disconnect", function(){
